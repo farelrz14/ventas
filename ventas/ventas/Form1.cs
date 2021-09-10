20998460ae60ac2905ea5bb16e9c3dca752fe278
farelrz14 <67718586+farelrz14@users.noreply.github.com>
2021-09-09 19:21:01 -0600

    agregando frase motivadora

--- ventas/ventas/Form1.cs
+++ ventas/ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola mundo");
+            MessageBox.Show("Piensa, cree, sueña y atrevete");
         }
     }
 }
